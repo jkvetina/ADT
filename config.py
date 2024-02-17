1fbd45cfa8fbe31759330e6dc81ccef8412388e7
Jan Kvetina <jan.kvetina@gmail.com>
2024-02-17 11:21:42 +0100

    Fix YAML files indentation

--- config.py
+++ config.py
@@ -176,7 +176,10 @@ class Config(Attributed):
 
         # store connection parameters in the yaml file
         with open(file, 'wt', encoding = 'utf-8') as f:
-            yaml.dump(payload, f, allow_unicode = True, default_flow_style = False)
+            # convert dict to yaml string
+            payload = yaml.dump(payload, allow_unicode = True, default_flow_style = False, indent = 4) + '\n'
+            payload = self.fix_yaml(payload)
+            f.write(payload)
             self.header('FILE CREATED:')
             print('   {}\n'.format(file))
 
@@ -295,6 +298,46 @@ class Config(Attributed):
     def fix_path(self, dir):
         return os.path.normpath(dir).replace('\\', '/').rstrip('/') + '/'
 
+
+
+    def fix_yaml(self, payload):
+        # change yaml formatting
+        recent  = {}
+        lines   = payload.splitlines()
+        #
+        for i, line in enumerate(lines):
+            # calculate indentation for current and next line
+            found = re.search('^(\s*)', line).group(1)
+            if not found:
+                continue
+            #
+            curr_line_indent = len(found)
+            #
+            if i >= len(lines):
+                next_line_indent = 0
+                continue
+            else:
+                next_line_indent = len(re.search('^(\s*)', lines[i + 1]).group(1))
+
+            # split current line
+            found = re.search('^(\s*)([^:]+)[:](.*)$', line)
+            if found and curr_line_indent > 0:
+                recent[i] = [found.group(1), found.group(2), found.group(3)]
+
+            # fix previous lines when end of group is detected
+            if next_line_indent != curr_line_indent:
+                # calculate the width of attributes
+                width = 0
+                for j, split in recent.items():
+                    width = max(width, int(len(split[1]) / 4) * 4 + 4)
+                #
+                for j, split in recent.items():
+                    split[1] = split[1].ljust(width)
+                    lines[j] = '{}{}:{}'.format(*split)
+                #
+                recent = {}
+        #
+        return '\n'.join(lines)
 
 
     def debug_dots(self, payload, length):

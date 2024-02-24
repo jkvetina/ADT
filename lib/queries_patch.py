eeb7e768512190a23e39b682a9acb50f8dd64bd0
Jan Kvetina <jan.kvetina@gmail.com>
2024-02-24 13:36:56 +0100

    Dont change version if NULL

--- lib/queries_patch.py
+++ lib/queries_patch.py
@@ -1,12 +1,19 @@
 query_apex_version = """
 BEGIN
+    -- set workspace so we can actually import APEX
     APEX_UTIL.SET_WORKSPACE (
         p_workspace         => '{$APEX_WORKSPACE}'
     );
-    APEX_APPLICATION_ADMIN.SET_APPLICATION_VERSION (
-        p_application_id    => {$APEX_APP_ID},
-        p_version           => '{$APEX_VERSION}'
-    );
+
+    -- set new version
+    IF '{$APEX_VERSION}' IS NOT NULL THEN
+        APEX_APPLICATION_ADMIN.SET_APPLICATION_VERSION (
+            p_application_id    => {$APEX_APP_ID},
+            p_version           => '{$APEX_VERSION}'
+        );
+    END IF;
+
+    -- keep sessions alive
     APEX_APPLICATION_INSTALL.SET_KEEP_SESSIONS(p_keep_sessions => TRUE);
     COMMIT;
 END;

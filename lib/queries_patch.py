36976991cb628b5fa7d6ec150eca2394f4721007
Jan Kvetina <jan.kvetina@gmail.com>
2024-03-23 11:34:55 +0100

    More templates

--- lib/queries_patch.py
+++ lib/queries_patch.py
@@ -50,7 +50,77 @@ BEGIN
     IF v_found IS NULL THEN
         EXECUTE IMMEDIATE
             '{$STATEMENT}';
-    END IF:
+    END IF;
+END;
+/
+"""
+
+# add new column to the table
+templates['ALTER | ADD COLUMN'] = """
+PROMPT "-- {$HEADER}";
+DECLARE
+    in_table_name       CONSTANT VARCHAR2(256) := '{$OBJECT_NAME}';
+    in_column_name      CONSTANT VARCHAR2(256) := '{$CC_NAME}';
+    --
+    v_found CHAR;
+BEGIN
+    SELECT MAX('Y') INTO v_found
+    FROM user_tab_columns
+    WHERE table_name    = in_table_name
+        AND column_name = in_column_name;
+    --
+    IF v_found IS NULL THEN
+        EXECUTE IMMEDIATE
+            '{$STATEMENT}';
+    END IF;
+END;
+/
+"""
+
+# add new column to the table
+templates['ALTER | DROP COLUMN'] = """
+PROMPT "-- {$HEADER}";
+DECLARE
+    in_table_name       CONSTANT VARCHAR2(256) := '{$OBJECT_NAME}';
+    in_column_name      CONSTANT VARCHAR2(256) := '{$CC_NAME}';
+    --
+    v_found CHAR;
+BEGIN
+    SELECT MAX('Y') INTO v_found
+    FROM user_tab_columns
+    WHERE table_name    = in_table_name
+        AND column_name = in_column_name;
+    --
+    IF v_found = 'Y' THEN
+        EXECUTE IMMEDIATE
+            '{$STATEMENT}';
+    END IF;
+END;
+/
+"""
+
+# add new constraint to the table
+templates['ALTER | ADD CONSTRAINT'] = """
+PROMPT "-- {$HEADER}";
+DECLARE
+    in_table_name           CONSTANT VARCHAR2(256) := '{$OBJECT_NAME}';
+    in_constraint_name      CONSTANT VARCHAR2(256) := '{$CC_NAME}';
+    --
+    v_found CHAR;
+BEGIN
+    SELECT MAX('Y') INTO v_found
+    FROM user_constraints
+    WHERE table_name        = in_table_name
+        AND constraint_name = in_constraint_name;
+    --
+    IF v_found IS NULL THEN
+        EXECUTE IMMEDIATE
+            '{$STATEMENT}';
+    END IF;
+END;
+/
+"""
+
 END;
 /
 """

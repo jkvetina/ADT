900c7d90933ece97d122864a1d5e32d872e9690f
Jan Kvetina <jan.kvetina@gmail.com>
2025-02-15 19:53:36 +0100

    Improve checks

--- config/patch_template/db_end/90_checks.sql
+++ config/patch_template/db_end/90_checks.sql
@@ -1,5 +1,8 @@
-
 -- show invalid objects
+PROMPT "INVALID OBJECTS:"
+COLUMN object_type FORMAT A10
+COLUMN object_name FORMAT A30
+--
 SELECT
     o.object_type,
     o.object_name
@@ -9,13 +12,54 @@ WHERE 1 = 1
     AND o.status        = 'INVALID'
 ORDER BY 1, 2;
 
+
+
+-- recompile invalid objects
+PROMPT "RECOMPILE INVALID OBJECTS..."
+BEGIN
+    DBMS_UTILITY.COMPILE_SCHEMA (
+        schema          => SYS_CONTEXT('USERENV', 'CURRENT_SCHEMA'),
+        compile_all     => FALSE
+    );
+END;
+/
+
+
+
+-- show invalid objects
+PROMPT "INVALID OBJECTS:"
+COLUMN object_type FORMAT A10
+COLUMN object_name FORMAT A30
+--
+SELECT
+    o.object_type,
+    o.object_name
+FROM user_objects o
+WHERE 1 = 1
+    AND o.object_name   LIKE '%'
+    AND o.status        = 'INVALID'
+ORDER BY 1, 2;
+
+
+
 -- check recent errors
-SELECT e.name, e.type, e.line, e.text
+PROMPT "RECENT ERRORS:"
+COLUMN object_type FORMAT A10
+COLUMN object_name FORMAT A30
+COLUMN line FORMAT A5
+COLUMN text FORMAT A30
+--
+SELECT
+    e.type AS object_type,
+    e.name AS object_name,
+    e.line,
+    e.text
 FROM user_errors e
 WHERE 1 = 1
     AND e.name  LIKE '%'
     AND e.text  NOT LIKE 'PLW%'
 ORDER BY
+    e.type,
     e.name,
     e.sequence;
 
